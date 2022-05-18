de135dbe2d98dbf5b7d38cddfc6f4f5985c6c69c
syper_olao <70114083+SyperOlao@users.noreply.github.com>
2022-05-19 02:45:33 +0300

    drawing interface

--- app/application.py
+++ app/application.py
@@ -1,49 +1,70 @@
 import tkinter as tk
 import matplotlib
+import numpy as np
 
 matplotlib.use('TkAgg')
 
 from matplotlib.figure import Figure
-from matplotlib.backends.backend_tkagg import (
-    FigureCanvasTkAgg,
-    NavigationToolbar2Tk
-)
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 
 
-class App(tk.Tk):
-    def __init__(self):
-        super().__init__()
+class Application:
+    def __init__(self, main_title):
+        self.window = tk.Tk()
+        self.window.title(main_title)
+        self.window.geometry('1280x720')
+        label1 = tk.Label(text="Rocket flying graph", fg="#eee", bg="#333")
+        label1.pack()
 
-        self.title('Tkinter Matplotlib Demo')
+        f_top = tk.LabelFrame(text="Data For Graph")
 
-        # prepare data
-        data = {
-            'Python': 11.27,
-            'C': 11.16,
-            'Java': 10.46,
-            'C++': 7.5,
-            'C#': 5.26
-        }
-        languages = data.keys()
-        popularity = data.values()
+        label2 = tk.Label(f_top, text="Enter vector_r ex (1 2 3)")
+        label2.pack(side=tk.TOP, expand=1)
 
+        self.txt1 = tk.Entry(f_top, width=30)
+        self.txt1.pack(side=tk.TOP, expand=1)
+
+        label3 = tk.Label(f_top, text="Enter vector_v ex (1 2 3)")
+        label3.pack(side=tk.TOP, expand=1)
+
+        self.txt2 = tk.Entry(f_top, width=30)
+        self.txt2.pack(side=tk.TOP, expand=1)
+        f_top.pack(padx=10, pady=10)
         # create a figure
+
+        self.add_button('rerender', self.print_graphics)
+
+    def print_graphics(self):
+
+        vector_v = self.get_vector(self.txt1.get())
+        vector_r = self.get_vector(self.txt2.get())
         figure = Figure(figsize=(6, 4), dpi=100)
 
         # create FigureCanvasTkAgg object
-        figure_canvas = FigureCanvasTkAgg(figure, self)
+        figure_canvas = FigureCanvasTkAgg(figure, self.window)
 
         # create the toolbar
-        NavigationToolbar2Tk(figure_canvas, self)
+        NavigationToolbar2Tk(figure_canvas, self.window)
 
         # create axes
         axes = figure.add_subplot()
 
         # create the barchart
-        axes.bar(languages, popularity)
-        axes.set_title('Top 5 Programming Languages')
-        axes.set_ylabel('Popularity')
+        axes.bar(vector_v, vector_r)
+        axes.set_title('vector_v')
+        axes.set_ylabel('vector_r')
 
         figure_canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
+    @staticmethod
+    def get_vector(sting_vector):
+        return np.array([int(s) for s in sting_vector.split() if s.isdigit()])
+
+    def rerender(self):
+        self.window.mainloop()
+
+    def add_button(self, text, onclick_f):
+        btn = tk.Button(self.window, text=text, command=onclick_f, bg='orange')
+
+        btn.pack(side=tk.BOTTOM, expand=1)
 
--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 # -*- coding: utf-8 -*-
 from application import *
-import numpy as np
-from Vizualization import show_trajectory
+
 
 
 if __name__ == '__main__':
-    app = App()
-    app.mainloop()
+    app = Application('Rocket flying')
+    app.rerender()
 
 
-vector_v = input()
-vector_v = np.array([int(s) for s in vector_v.split() if s.isdigit()])
-
-vector_r = input()
-vector_r = np.array([int(s) for s in vector_r.split() if s.isdigit()])
