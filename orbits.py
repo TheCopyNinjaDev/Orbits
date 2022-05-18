b384e774a3288bcc137c0f846d76908756786d26
unknown <zolotarev_a_a@mail.ru>
2022-05-18 23:47:50 +0300

    get_orbit_parameters

--- orbits.py
+++ orbits.py
@@ -1,6 +1,6 @@
 import numpy as np
-
 from typing import Tuple
+import math
 
 
 def get_orbit_parameters(v_vector: np.array, r_vector: np.array) -> Tuple[float, float]:
@@ -18,17 +18,35 @@ def get_orbit_parameters(v_vector: np.array, r_vector: np.array) -> Tuple[float,
     # mu coefficient
     MU = G * ME
     # Vector H
-    h_vector = np.cross(v_vector, r_vector)
+    h_vector = np.int64(np.cross(v_vector, r_vector))
+
+    # Square the elements of vector H
+    h_vector_square = np.array([i ** 2 for i in h_vector], dtype=np.int64)
+    # Find the sum of the elements of the vector H
+    h_vector_sum = np.sum(h_vector_square, dtype=np.int64)
     # Vector length H
-    h = np.sum([i**2 for i in h_vector])**(1/2)
+    h = math.sqrt(h_vector_sum)
     # Focal parameter
-    p = h**2 / MU
+    p = np.int64(h**2) / MU
+
+    # Square the elements of the vector R
+    r_vector_square = np.array([i ** 2 for i in r_vector], dtype=np.int64)
+    # Find the sum of the elements of the vector H
+    r_vector_sum = np.sum(r_vector_square, dtype=np.int64)
     # Vector length R
-    r = np.sum([i**2 for i in r_vector])**(1/2)
+    r = math.sqrt(r_vector_sum)
+
+    # Find the vector product of vectors H and R
+    vector_h_v = np.int64(np.cross(v_vector, h_vector))
+
     # Vector eccentricity
-    e_vector = np.cross(v_vector, h_vector) / MU - r_vector / r
-    # Vector length eccentricity
-    e = np.sum([i**2 for i in e_vector])**(1/2)
+    e_vector = vector_h_v / MU - r_vector / r
+    # Square the elements of the vector R
+    e_vector_square = np.array([i ** 2 for i in e_vector], dtype=np.float64)
+    # Find the sum of the elements of the vector H
+    e_vector_sum = np.sum(e_vector_square, dtype=np.float64)
+    # Vector length R
+    e = math.sqrt(e_vector_sum)
     return e, p
 
 
